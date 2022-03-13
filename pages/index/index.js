194c2048c8898611e6746d3dcc5a9755eea9f362
timeofmei <timeofmei@outlook.com>
2022-03-13 22:10:25 +0800

    首页，搜索功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,60 @@
+const app = getApp()
+
 Page({
   data: {
-    courses: []
+    courses: [],
+  },
+
+  processScores(score) {
+    if (Number.isInteger(score)) {
+      return String(score) + '.0'
+    } else {
+      return String(Math.round(score, 1))
+    }
+  },
+
+  processResult(courses) {
+    for (let i = 0; i < courses.length; i++) {
+      courses[i].course_rate = this.processScores(courses[i].course_rate)
+      courses[i].course_color = courses[i].course_rate >= 4 ? 'green' : courses[i].course_rate >= 3 ? 'yellow' : 'red'
+    }
+    return courses
+  },
+
+  searchEmpty() {
+    this.search('')
+  },
+
+  search(e) {
+    let keyword = e?.detail?.value ?? e
+    if (keyword === '') {
+      keyword = '院'
+    }
+    wx.request({
+      method: 'POST',
+      header: {
+        'Cookie': 'csrftoken=' + app.globalData.csrftoken,
+        'content-type': 'application/x-www-form-urlencoded',
+        'hello': 'world'
+      },
+      url: 'https://course.shattered.ink/course/search/',
+      data: {
+        csrfmiddlewaretoken: app.globalData.csrftoken,
+        keywords: keyword,
+        page: 1
+      },
+      success: (res) => {
+        this.setData({
+          courses: this.processResult(res.data.data)
+        })
+      }
+    })
+  },
+
+  toDetail() {
+    wx.navigateTo({
+      url: '../detail/detail',
+    })
   },
 
   toAdd() {
@@ -11,35 +65,26 @@ Page({
 
   onLoad() {
     console.log("Loading Page: Index")
+    new Promise((resolve, reject) => {
+        wx.request({
+          url: 'https://course.shattered.ink/token/',
+          success: (res) => {
+            let result = res.cookies[0].substring(res.cookies[0].indexOf('=') + 1, res.cookies[0].indexOf(';'))
+            resolve(result);
+          },
+          fail: (res) => {
+            reject(res)
+          }
+        })
+      })
+      .then((res) => {
+        app.globalData.csrftoken = res
+        this.searchEmpty()
+      })
   },
 
   onReady() {
-    let basic = [
-      {
-        id: 0,
-        name: "数理统计",
-        description: "经济学院统计系必修课"
-      },
-      {
-        id: 0,
-        name: "常微分方程",
-        description: "老师比较帅"
-      },
-      {
-        id: 0,
-        name: "贝叶斯统计",
-        description: "老师不是很帅"
-      }
-    ]
-    let courses = [];
-    for (let i = 0; i < 15; i++) {
-      courses.push({...basic[i % basic.length], id: i})
-    }
-    this.setData({
-      courses: courses
-    })
     console.log("Page Index Ready")
     console.log(new Date().toTimeString())
   }
-
 })
